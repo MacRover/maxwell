f01cb77091a1165d838d8baade0a324162c0887c
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2023-12-22 07:36:16 -0500

    basic CAN sub, send to bus

--- ros_ws/src/spidercan/model/ggoblin.py
+++ ros_ws/src/spidercan/model/ggoblin.py
@@ -1,15 +1,39 @@
 #!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
 
 from can import Message
 from can.interface import Bus
 
+from custom_interfaces.msg import CANraw
 
-def main():
-    bus = Bus(interface="socketcan", channel="can0", bitrate=500000)
-    message = Message(
-        is_extended_id=True, arbitration_id=0xC0FFEE, data=b"hi world"
-    )
-    bus.send(message)
+
+class GGoblin(Node):
+    def __init__(self):
+        super().__init__("ggoblin")
+        self.subscription = self.create_subscription(
+            CANraw, "/can/can_out", self.listener_callback, 10
+        )
+        self.subscription
+
+    def listener_callback(self, msg):
+        # self.get_logger().info('I heard: "%d"' % msg.data)  # CHANGE
+        print(msg)
+        bus = Bus(interface="socketcan", channel="can0", bitrate=500000)
+        message = Message(
+            is_extended_id=msg.extended, arbitration_id=msg.address, data=msg.data
+        )
+        bus.send(message)
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    ggoblin = GGoblin()
+    rclpy.spin(ggoblin)
+
+    ggoblin.destroy_node()
+    rclpy.shutdown()
 
 
 if __name__ == "__main__":
