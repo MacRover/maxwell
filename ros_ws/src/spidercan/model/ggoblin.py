fd01db1109f1df9030b327e0898aef3734760bf4
emcauliffe <14607273+emcauliffe@users.noreply.github.com>
2023-12-23 22:44:55 -0500

    undelete can tx code

--- ros_ws/src/spidercan/model/ggoblin.py
+++ ros_ws/src/spidercan/model/ggoblin.py
@@ -1,1 +1,40 @@
- 
+#!/usr/bin/env python3
+import rclpy
+from rclpy.node import Node
+
+from can import Message, ThreadSafeBus
+
+from custom_interfaces.msg import CANraw
+
+
+class GGoblin(Node):
+    def __init__(self, bus, topic):
+        super().__init__("ggoblin")
+        self.bus = bus
+        self.topic = topic
+        self.subscription = self.create_subscription(
+            CANraw, self.topic, self.listener_callback, 10
+        )
+
+    def listener_callback(self, msg):
+        message = Message(
+            is_extended_id=msg.extended, arbitration_id=msg.address, data=msg.data
+        )
+        self.bus.send(message)
+        self.get_logger().info(f"CAN TX addr:{msg.address}, data:{msg.data}")
+
+
+def main(args=None):
+    rclpy.init(args=args)
+
+    bus = ThreadSafeBus(interface="socketcan", channel="can0", bitrate=500000)
+    ggoblin = GGoblin(bus=bus, topic="/can/can_out")
+    rclpy.spin(ggoblin)
+
+    bus.shutdown()
+    ggoblin.destroy_node()
+    rclpy.shutdown()
+
+
+if __name__ == "__main__":
+    main()
