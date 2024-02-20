06a08ded21c8133b3440936aa61d677131c99ab5
chosterto <johnnychox@gmail.com>
2024-02-20 14:35:32 -0500

    publish separate joy msgs

--- ros_ws/src/control/control/overlay.py
+++ ros_ws/src/control/control/overlay.py
@@ -4,28 +4,32 @@ from rclpy.node import Node
 from sensor_msgs.msg import Joy
 from custom_interfaces.msg import ControllerMode
 
+DRIVE_MODE = 0
+ARM_MODE = 1
+
 class Toggler(Node):
     def __init__(self):
         super().__init__("manager")
-        self.pub = self.create_publisher(ControllerMode, "/mode", 10)
+        self.drive = self.create_publisher(Joy, "/joy_drive", 10)
+        self.arm = self.create_publisher(Joy, "/joy_arm", 10)
         self.create_subscription(Joy, "/joy", self._toggle_mode_callback, 10)
 
         self.active = False
         self.state = 0
-        self.num = 2
 
     def _toggle_mode_callback(self, msg):
         if (msg.buttons[5] and not self.active):
             self.active = True
-            self.state = (self.state + 1) % self.num
+            self.state = (self.state + 1) % 2
         elif (not msg.buttons[5]):
             self.active = False
 
-        modes = ControllerMode(
-            drivetrain=(self.state == 0),
-            arm=(self.state == 1)
-        )
-        self.pub.publish(modes)
+        if (self.state == DRIVE_MODE):
+            self.drive.publish(msg)
+        elif (self.state == ARM_MODE):
+            self.arm.publish(msg)
+        else:
+            self.drive.publish(msg)
 
 
 def main(args=None):
