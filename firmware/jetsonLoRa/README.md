f423d2a529d3c472ce896614bf227184280fd491
Vaibhav Gopal <vabsgop@gmail.com>
2024-03-08 19:08:30 -0500

    Added jetson LoRa testing code

---
+++ firmware/jetsonLoRa/README.md
@@ -0,0 +1,1 @@
+# LoRa Ping Pong Firmware
---
+++ firmware/jetsonLoRa/device 1/main.py
@@ -0,0 +1,36 @@
+from sx1262 import SX1262
+import time
+
+def cb(events):
+    if events & SX1262.RX_DONE:
+        msg, err = sx.recv()
+        error = SX1262.STATUS[err]
+        print('Receive: {}, {}'.format(msg, error))
+    elif events & SX1262.TX_DONE:
+        print('TX done.')
+
+# for jetson orin nano, must change cs, irq, rst, and gpio depending on what pins the LoRa chip is connected to.
+# CS pin must be connected the NSS pin on the LoRa chip
+sx = SX1262(spi_bus=1, clk=13, mosi=37, miso=22, cs=18, irq=20, rst=15, gpio=2) 
+
+# LoRa
+sx.begin(freq=923, bw=500.0, sf=12, cr=8, syncWord=0x12,
+         power=-5, currentLimit=60.0, preambleLength=8,
+         implicit=False, implicitLen=0xFF,
+         crcOn=True, txIq=False, rxIq=False,
+         tcxoVoltage=1.7, useRegulatorLDO=False, blocking=True)
+
+# FSK
+##sx.beginFSK(freq=923, br=48.0, freqDev=50.0, rxBw=156.2, power=-5, currentLimit=60.0,
+##            preambleLength=16, dataShaping=0.5, syncWord=[0x2D, 0x01], syncBitsLength=16,
+##            addrFilter=SX126X_GFSK_ADDRESS_FILT_OFF, addr=0x00, crcLength=2, crcInitial=0x1D0F, crcPolynomial=0x1021,
+##            crcInverted=True, whiteningOn=True, whiteningInitial=0x0100,
+##            fixedPacketLength=False, packetLength=0xFF, preambleDetectorLength=SX126X_GFSK_PREAMBLE_DETECT_16,
+##            tcxoVoltage=1.6, useRegulatorLDO=False,
+##            blocking=True)
+
+sx.setBlockingCallback(False, cb)
+
+while True:
+    sx.send(b'Ping')
+    time.sleep(10)
