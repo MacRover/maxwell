6d845f3b76ab9f75f421c29d706ae4f13a161667
chosterto <johnnychox@gmail.com>
2024-05-14 13:03:49 -0400

    test VESC controller

--- ros_ws/src/drive/drive/VESC.py
+++ ros_ws/src/drive/drive/VESC.py
@@ -1,18 +1,27 @@
 import struct
+from math import pi
+from enum import Enum
 from custom_interfaces.msg import CANraw
 
-CAN_PACKET_SET_DUTY = 0
-CAN_PACKET_SET_CURRENT = 1
-CAN_PACKET_SET_CURRENT_BRAKE = 2
-CAN_PACKET_SET_RPM = 3
-CAN_PACKET_SET_POS = 4
+class CMD(Enum):
+    CAN_PACKET_SET_DUTY = 0
+    CAN_PACKET_SET_CURRENT = 1
+    CAN_PACKET_SET_CURRENT_BRAKE = 2
+    CAN_PACKET_SET_RPM = 3
+    CAN_PACKET_SET_POS = 4
+
 
 class VESC:
+    """
+    VESC class that converts commands into CAN messages
+
+    vesc_id: can id of VESC
+    """
     def __init__(self, vesc_id: int):
         self.id = vesc_id
         self.can = CANraw()
         self.scaling = 1
-        self.cmd_id = CAN_PACKET_SET_RPM
+        self.cmd_id = CMD.CAN_PACKET_SET_RPM
         self.data = 0
     
     def get_can_message(self) -> CANraw:
@@ -21,23 +30,28 @@ class VESC:
         return self.can
     
     def set_duty(self, duty_cycle: int) -> None:
-        self.cmd_id = CAN_PACKET_SET_DUTY
+        self.cmd_id = CMD.CAN_PACKET_SET_DUTY
         self.data = duty_cycle
         self.scaling = 100000
 
     def set_current(self, current: int, brake: bool) -> None:
-        self.cmd_id = (CAN_PACKET_SET_CURRENT_BRAKE if brake else 
-                       CAN_PACKET_SET_CURRENT)
+        self.cmd_id = (CMD.CAN_PACKET_SET_CURRENT_BRAKE if brake else 
+                       CMD.CAN_PACKET_SET_CURRENT)
         self.data = current
         self.scaling = 1000
     
     def set_rpm(self, rpm: int) -> None:
-        self.cmd_id = CAN_PACKET_SET_RPM
+        self.cmd_id = CMD.AN_PACKET_SET_RPM
         self.data = rpm
         self.scaling = 1
     
+    def set_speed_mps(self, speed: float) -> None:
+        d = 1.0
+        mps_to_rpm = round((speed / (pi*d)) * 60.0)
+        self.set_rpm(mps_to_rpm)
+    
     def set_pos(self, deg: int) -> None:
-        self.cmd_id = CAN_PACKET_SET_POS
+        self.cmd_id = CMD.CAN_PACKET_SET_POS
         self.data = deg
         self.scaling = 1000000
     
--- ros_ws/src/drive/drive/vesc_controller.py
+++ ros_ws/src/drive/drive/vesc_controller.py
@@ -1,9 +1,17 @@
 #! /usr/bin/env python3
 import rclpy
 from rclpy.node import Node
+from enum import Enum
 
 from custom_interfaces.msg import *
 from .VESC import VESC
+
+class VESC_ID(Enum):
+    FRONT_LEFT = 0
+    FRONT_RIGHT = 1
+    BACK_LEFT = 2
+    BACK_RIGHT = 3
+
 
 class VescController(Node):
     def __init__(self):
@@ -14,9 +22,22 @@ class VescController(Node):
             self._callback, 10)
         
         self.pub = self.create_publisher(CANraw, "/can/can_out", 10)
+
+        self.front_left = VESC(VESC_ID.FRONT_LEFT)
+        self.front_right = VESC(VESC_ID.FRONT_RIGHT)
+        self.back_left = VESC(VESC_ID.BACK_LEFT)
+        self.back_right = VESC(VESC_ID.BACK_RIGHT)
     
     def _callback(self, msg):
-        pass
+        self.front_left.set_speed_mps(msg[0].speed)
+        self.front_right.set_speed_mps(msg[1].speed)
+        self.back_left.set_speed_mps(msg[2].speed)
+        self.back_right.set_speed_mps(msg[3].speed)
+
+        self.pub.publish(front_left.get_can_message())
+        self.pub.publish(front_right.get_can_message())
+        self.pub.publish(back_left.get_can_message())
+        self.pub.publish(back_right.get_can_message())
 
 
 def main(args=None):
