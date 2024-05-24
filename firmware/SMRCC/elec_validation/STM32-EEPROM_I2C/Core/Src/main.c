0adc00afd7e9b41c616014a1f92e2b9efee47049
Ishan Vermani <ishanvermani65@gmail.com>
2024-05-23 23:17:39 -0400

    SMRCC I2C Support

--- firmware/SMRCC/elec_validation/STM32-EEPROM_I2C/Core/Src/main.c
+++ firmware/SMRCC/elec_validation/STM32-EEPROM_I2C/Core/Src/main.c
@@ -71,49 +71,49 @@ static void MX_I2C2_Init(void);
 
 void setupTxCAN()
 {
-    TxHeader.StdId = 0x321;
-    TxHeader.ExtId = 0x01;
-    TxHeader.RTR = CAN_RTR_DATA;
-    TxHeader.IDE = CAN_ID_EXT;
-    TxHeader.DLC = 8;
-    TxHeader.TransmitGlobalTime = DISABLE;
+  TxHeader.StdId = 0x321;
+  TxHeader.ExtId = 0x01;
+  TxHeader.RTR = CAN_RTR_DATA;
+  TxHeader.IDE = CAN_ID_EXT;
+  TxHeader.DLC = 8;
+  TxHeader.TransmitGlobalTime = DISABLE;
 
-    TxData[0] = 0xA0;
-    TxData[1] = 0xA1;
-    TxData[2] = 0xA2;
-    TxData[3] = 0xA3;
-    TxData[4] = 0xA4;
-    TxData[5] = 0xA5;
-    TxData[6] = 0xA6;
-    TxData[7] = 0xA7;
+  TxData[0] = 0xA0;
+  TxData[1] = 0xA1;
+  TxData[2] = 0xA2;
+  TxData[3] = 0xA3;
+  TxData[4] = 0xA4;
+  TxData[5] = 0xA5;
+  TxData[6] = 0xA6;
+  TxData[7] = 0xA7;
 }
 
 void setupRxCAN()
 {
-    canfilterconfig.FilterBank = 0;
-    canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
-    canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
-    canfilterconfig.FilterIdHigh = 0x0000;
-    canfilterconfig.FilterIdLow = 0x0000;
-    canfilterconfig.FilterMaskIdHigh = 0x0000;
-    canfilterconfig.FilterMaskIdLow = 0x0000;
-    canfilterconfig.FilterFIFOAssignment = CAN_RX_FIFO0;
-    canfilterconfig.FilterActivation = ENABLE;
-    canfilterconfig.SlaveStartFilterBank = 14;
+  canfilterconfig.FilterBank = 0;
+  canfilterconfig.FilterMode = CAN_FILTERMODE_IDMASK;
+  canfilterconfig.FilterScale = CAN_FILTERSCALE_32BIT;
+  canfilterconfig.FilterIdHigh = 0x0000;
+  canfilterconfig.FilterIdLow = 0x0000;
+  canfilterconfig.FilterMaskIdHigh = 0x0000;
+  canfilterconfig.FilterMaskIdLow = 0x0000;
+  canfilterconfig.FilterFIFOAssignment = CAN_RX_FIFO0;
+  canfilterconfig.FilterActivation = ENABLE;
+  canfilterconfig.SlaveStartFilterBank = 14;
 
-    HAL_CAN_ConfigFilter(&hcan, &canfilterconfig);
-    HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING); //enable interrupts
+  HAL_CAN_ConfigFilter(&hcan, &canfilterconfig);
+  HAL_CAN_ActivateNotification(&hcan, CAN_IT_RX_FIFO0_MSG_PENDING); // enable interrupts
 }
 
 void txCAN()
 {
-    TxHeader.DLC = 8;
-    HAL_StatusTypeDef can_result = HAL_CAN_AddTxMessage(&hcan, &TxHeader,
-            TxData, &TxMailbox);
-    if (can_result != HAL_OK)
-    {
-        Error_Handler();
-    }
+  TxHeader.DLC = 8;
+  HAL_StatusTypeDef can_result = HAL_CAN_AddTxMessage(&hcan, &TxHeader,
+                                                      TxData, &TxMailbox);
+  if (can_result != HAL_OK)
+  {
+    Error_Handler();
+  }
 }
 
 /**
@@ -124,27 +124,26 @@ void txCAN()
  */
 void HAL_CAN_RxFifo0MsgPendingCallback(CAN_HandleTypeDef *CanHandle)
 {
-    uint8_t echo_data[8];
-    /* Get RX message */
-    if (HAL_CAN_GetRxMessage(CanHandle, CAN_RX_FIFO0, &RxHeader, RxData)
-            != HAL_OK)
-    {
-        /* Reception Error */
-        Error_Handler();
-    }
+  uint8_t echo_data[8];
+  /* Get RX message */
+  if (HAL_CAN_GetRxMessage(CanHandle, CAN_RX_FIFO0, &RxHeader, RxData) != HAL_OK)
+  {
+    /* Reception Error */
+    Error_Handler();
+  }
 
-    HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
-    // Echo received CAN message
-    HAL_CAN_AddTxMessage(&hcan, &TxHeader, echo_data, &TxMailbox);
-
+  HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
+  // Echo received CAN message
+  HAL_CAN_AddTxMessage(&hcan, &TxHeader, echo_data, &TxMailbox);
 }
+
 
 /* USER CODE END 0 */
 
 /**
-  * @brief  The application entry point.
-  * @retval int
-  */
+ * @brief  The application entry point.
+ * @retval int
+ */
 int main(void)
 {
   /* USER CODE BEGIN 1 */
@@ -172,89 +171,190 @@ int main(void)
   MX_CAN_Init();
   MX_I2C2_Init();
   /* USER CODE BEGIN 2 */
-    setupTxCAN();
-    setupRxCAN();
+  setupTxCAN();
+  setupRxCAN();
   /* USER CODE END 2 */
 
   /* Infinite loop */
   /* USER CODE BEGIN WHILE */
-    while (1)
+
+  while (1)
+  {
+    txCAN();
+    //        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
+
+    // set channel
+    uint8_t device_identifier = 0b11100000;
+    uint8_t a2_pin = 0;
+    uint8_t a1_pin = 0;
+    uint8_t a0_pin = 0;
+    uint8_t read_write = 0; // write
+
+    uint8_t i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2) | (a0_pin << 1) | read_write;
+
+    uint8_t channel = 2;
+
+    uint8_t i2c_send_data_mux = 0b00000000 | (channel & 3);
+
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address, &i2c_send_data_mux, 1, 10000) != HAL_OK)
     {
-        txCAN();
-//        HAL_GPIO_TogglePin(GPIOB, GPIO_PIN_2);
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
 
-        uint8_t read_eeprom = 0; // address in eeprom to write to
-        uint16_t eeprom_address = 321;
+    //shift i/o mux
+    //configure input/output
+    device_identifier = 0b00110000;
+    a2_pin = 0;
+    a1_pin = 0;
+    a0_pin = 0;
+    read_write = 0; // write
 
-        uint8_t device_identifier = 0b10100000;
-        uint8_t a2_pin = 0;
-        uint8_t a1_pin = 0;
+    i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2) | (a0_pin << 1) | read_write;
 
-        uint8_t i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2)
-                | (eeprom_address >> 8) << 1 | read_eeprom;
+    uint8_t i2c_send_data_io[2];
+    i2c_send_data_io[0] = (uint8_t )0b00000011; //configuration register
+    i2c_send_data_io[1] = (uint8_t) 0b00011111; // 7-5 output, 1-4 input, 0 NC
 
-        uint8_t i2c_send_data[2];
-        i2c_send_data[0] = (uint8_t) eeprom_address & 0xff;
-        i2c_send_data[1] = (uint8_t) 0x79;
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address, i2c_send_data_io, 2, 10000) != HAL_OK)
+    {
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
 
-        while (HAL_I2C_Master_Transmit(&hi2c1, (uint16_t) i2c_address,
-                &i2c_send_data[0], sizeof(i2c_send_data), 10000) != HAL_OK)
-        {
-            /* Error_Handler() function is called when Timeout error occurs.
-             When Acknowledge failure occurs (Slave don't acknowledge its address)
-             Master restarts communication */
-            if (HAL_I2C_GetError(&hi2c1) != HAL_I2C_ERROR_AF)
-            {
-                Error_Handler();
-            }
-        }
+    //configure input polarity
 
-        HAL_Delay(25);
+    i2c_send_data_io[0] = (uint8_t) 0b00000010; //output port register
+    i2c_send_data_io[1] = (uint8_t) 0b00000000; // none are inverted
 
-        eeprom_address = 123; // address in eeprom to read from
-        i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2)
-                | (eeprom_address >> 8) << 1 | read_eeprom;
-        uint8_t i2c_address_write = (uint8_t) (i2c_address & 0xfe);
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address, i2c_send_data_io, 2, 10000) != HAL_OK)
+    {
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
 
-        uint8_t i2c_data_buff;
-        while (HAL_I2C_Mem_Read(&hi2c1, (uint16_t) i2c_address_write,
-                (uint16_t) eeprom_address, I2C_MEMADD_SIZE_8BIT, &i2c_data_buff,
-                1, 10000) != HAL_OK)
-        {
-            /* Error_Handler() function is called when Timeout error occurs.
-             When Acknowledge failure occurs (Slave don't acknowledge its address)
-             Master restarts communication */
-            if (HAL_I2C_GetError(&hi2c1) != HAL_I2C_ERROR_AF)
-            {
-                Error_Handler();
-            }
-        }
+    //configure output channels
+    i2c_send_data_io[0] = (uint8_t) 0b00000001; //output port register
+    i2c_send_data_io[1] = (uint8_t) 0b11100000; // 7-5 on
 
-        uint8_t can_data[1];
-        can_data[0] = i2c_data_buff;
-        TxHeader.DLC = 1;
-        HAL_CAN_AddTxMessage(&hcan, &TxHeader, can_data, &TxMailbox);
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address, i2c_send_data_io, 2, 10000) != HAL_OK)
+    {
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
 
-        HAL_Delay(500);
+    //configure register for reading
+    i2c_send_data_io[0] = (uint8_t) 0b00000000; //input port register
+    i2c_send_data_io[1] = (uint8_t) 0b11100000; // dont' care
+
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address, i2c_send_data_io, 2, 10000) != HAL_OK)
+    {
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
+
+    // write eeprom
+    uint8_t read_eeprom = 0; // address in eeprom to write to
+    uint16_t eeprom_address = 321;
+
+    device_identifier = 0b10100000;
+    a2_pin = 0;
+    a1_pin = 0;
+
+    i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2) | (eeprom_address >> 8) << 1 | read_eeprom;
+
+    uint8_t i2c_send_data[2];
+    i2c_send_data[0] = (uint8_t)eeprom_address & 0xff;
+    i2c_send_data[1] = (uint8_t)channel;
+
+    while (HAL_I2C_Master_Transmit(&hi2c2, (uint16_t)i2c_address,
+                                   i2c_send_data, sizeof(i2c_send_data), 10000) != HAL_OK)
+    {
+      /* Error_Handler() function is called when Timeout error occurs.
+       When Acknowledge failure occurs (Slave don't acknowledge its address)
+       Master restarts communication */
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
+
+    
+
+    HAL_Delay(25);
+
+    eeprom_address = 123; // address in eeprom to read from
+    i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2) | (eeprom_address >> 8) << 1 | read_eeprom;
+    uint8_t i2c_address_write = (uint8_t)(i2c_address & 0xfe);
+
+    uint8_t i2c_data_buff;
+    while (HAL_I2C_Mem_Read(&hi2c2, (uint16_t)i2c_address_write,
+                            (uint16_t)eeprom_address, I2C_MEMADD_SIZE_8BIT, &i2c_data_buff,
+                            1, 10000) != HAL_OK)
+    {
+      /* Error_Handler() function is called when Timeout error occurs.
+       When Acknowledge failure occurs (Slave don't acknowledge its address)
+       Master restarts communication */
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
+
+    //read io mux
+    device_identifier = 0b00110000;
+    a2_pin = 0;
+    a1_pin = 0;
+    a0_pin = 0;
+    read_write = 1; // read
+
+    i2c_address = device_identifier | (a2_pin << 3) | (a1_pin << 2) | (a0_pin << 1) | read_write;
+
+    uint8_t i2c_read_data_io;
+
+    while (HAL_I2C_Master_Receive(&hi2c2, (uint16_t)i2c_address, &i2c_read_data_io, 12, 10000) != HAL_OK)
+    {
+      if (HAL_I2C_GetError(&hi2c2) != HAL_I2C_ERROR_AF)
+      {
+        Error_Handler();
+      }
+    }
+
+    uint8_t can_data[1];
+    can_data[0] = i2c_data_buff;
+    TxHeader.DLC = 1;
+    HAL_CAN_AddTxMessage(&hcan, &TxHeader, can_data, &TxMailbox);
+
+    HAL_Delay(500);
     /* USER CODE END WHILE */
 
     /* USER CODE BEGIN 3 */
-    }
+  }
   /* USER CODE END 3 */
 }
 
 /**
-  * @brief System Clock Configuration
-  * @retval None
-  */
+ * @brief System Clock Configuration
+ * @retval None
+ */
 void SystemClock_Config(void)
 {
   RCC_OscInitTypeDef RCC_OscInitStruct = {0};
   RCC_ClkInitTypeDef RCC_ClkInitStruct = {0};
 
   /** Initializes the RCC Oscillators according to the specified parameters
-  * in the RCC_OscInitTypeDef structure.
-  */
+   * in the RCC_OscInitTypeDef structure.
+   */
   RCC_OscInitStruct.OscillatorType = RCC_OSCILLATORTYPE_HSI;
   RCC_OscInitStruct.HSIState = RCC_HSI_ON;
   RCC_OscInitStruct.HSICalibrationValue = RCC_HSICALIBRATION_DEFAULT;
@@ -267,9 +367,8 @@ void SystemClock_Config(void)
   }
 
   /** Initializes the CPU, AHB and APB buses clocks
-  */
-  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK|RCC_CLOCKTYPE_SYSCLK
-                              |RCC_CLOCKTYPE_PCLK1|RCC_CLOCKTYPE_PCLK2;
+   */
+  RCC_ClkInitStruct.ClockType = RCC_CLOCKTYPE_HCLK | RCC_CLOCKTYPE_SYSCLK | RCC_CLOCKTYPE_PCLK1 | RCC_CLOCKTYPE_PCLK2;
   RCC_ClkInitStruct.SYSCLKSource = RCC_SYSCLKSOURCE_PLLCLK;
   RCC_ClkInitStruct.AHBCLKDivider = RCC_SYSCLK_DIV1;
   RCC_ClkInitStruct.APB1CLKDivider = RCC_HCLK_DIV1;
@@ -282,10 +381,10 @@ void SystemClock_Config(void)
 }
 
 /**
-  * @brief CAN Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief CAN Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_CAN_Init(void)
 {
 
@@ -314,21 +413,20 @@ static void MX_CAN_Init(void)
   }
   /* USER CODE BEGIN CAN_Init 2 */
 
-    if (HAL_CAN_Start(&hcan) != HAL_OK)
-    {
-        /* Start Error */
-        Error_Handler();
-    }
+  if (HAL_CAN_Start(&hcan) != HAL_OK)
+  {
+    /* Start Error */
+    Error_Handler();
+  }
 
   /* USER CODE END CAN_Init 2 */
-
 }
 
 /**
-  * @brief I2C2 Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief I2C2 Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_I2C2_Init(void)
 {
 
@@ -355,19 +453,18 @@ static void MX_I2C2_Init(void)
   /* USER CODE BEGIN I2C2_Init 2 */
 
   /* USER CODE END I2C2_Init 2 */
-
 }
 
 /**
-  * @brief GPIO Initialization Function
-  * @param None
-  * @retval None
-  */
+ * @brief GPIO Initialization Function
+ * @param None
+ * @retval None
+ */
 static void MX_GPIO_Init(void)
 {
   GPIO_InitTypeDef GPIO_InitStruct = {0};
-/* USER CODE BEGIN MX_GPIO_Init_1 */
-/* USER CODE END MX_GPIO_Init_1 */
+  /* USER CODE BEGIN MX_GPIO_Init_1 */
+  /* USER CODE END MX_GPIO_Init_1 */
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOD_CLK_ENABLE();
@@ -384,8 +481,8 @@ static void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(GPIOB, &GPIO_InitStruct);
 
-/* USER CODE BEGIN MX_GPIO_Init_2 */
-/* USER CODE END MX_GPIO_Init_2 */
+  /* USER CODE BEGIN MX_GPIO_Init_2 */
+  /* USER CODE END MX_GPIO_Init_2 */
 }
 
 /* USER CODE BEGIN 4 */
@@ -393,28 +490,28 @@ static void MX_GPIO_Init(void)
 /* USER CODE END 4 */
 
 /**
-  * @brief  This function is executed in case of error occurrence.
-  * @retval None
-  */
+ * @brief  This function is executed in case of error occurrence.
+ * @retval None
+ */
 void Error_Handler(void)
 {
   /* USER CODE BEGIN Error_Handler_Debug */
-    /* User can add his own implementation to report the HAL error return state */
-    __disable_irq();
-    while (1)
-    {
-    }
+  /* User can add his own implementation to report the HAL error return state */
+  __disable_irq();
+  while (1)
+  {
+  }
   /* USER CODE END Error_Handler_Debug */
 }
 
-#ifdef  USE_FULL_ASSERT
+#ifdef USE_FULL_ASSERT
 /**
-  * @brief  Reports the name of the source file and the source line number
-  *         where the assert_param error has occurred.
-  * @param  file: pointer to the source file name
-  * @param  line: assert_param error line source number
-  * @retval None
-  */
+ * @brief  Reports the name of the source file and the source line number
+ *         where the assert_param error has occurred.
+ * @param  file: pointer to the source file name
+ * @param  line: assert_param error line source number
+ * @retval None
+ */
 void assert_failed(uint8_t *file, uint32_t line)
 {
   /* USER CODE BEGIN 6 */
