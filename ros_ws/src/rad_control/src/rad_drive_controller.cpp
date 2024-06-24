fed3a1aa363ede2ccb2867a97522df280bc24ea6
chosterto <johnnychox@gmail.com>
2024-06-24 14:09:07 -0400

    init rad control package

---
+++ ros_ws/src/rad_control/src/rad_drive_controller.cpp
@@ -0,0 +1,11 @@
+#include <cstdio>
+#include "rad_control/rad_drive_controller.hpp"
+
+int main(int argc, char ** argv)
+{
+  (void) argc;
+  (void) argv;
+
+  printf("hello world rad_control package\n");
+  return 0;
+}
