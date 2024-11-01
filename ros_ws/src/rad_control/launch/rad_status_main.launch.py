589f5b25dbd2762a947ae25db24f37632b6298e9
chosterto <johnnychox@gmail.com>
2024-11-01 01:06:53 -0400

    Config CAN reader

--- ros_ws/src/rad_control/launch/rad_status_main.launch.py
+++ ros_ws/src/rad_control/launch/rad_status_main.launch.py
@@ -15,6 +15,19 @@ def generate_launch_description():
         }]
     )
 
+    # CONFIG READER NODE
+    reader2_node = Node(
+        package="spidercan",
+        executable="reader.py",
+        name="rad2_reader",
+        parameters=[{
+            "topic": "/can/config/rad_can_in",
+            "channel": "can0",
+            "can_ids": [0x4040000],
+            "can_masks": [0xFFF8000]
+        }]
+    )
+
     rad_status_node = Node(
         package="rad_control",
         executable="rad_status",
