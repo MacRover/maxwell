ed67878c915752dbf6859b64954a0fc423cd9b80
Ishan Vermani <ishanvermani65@gmail.com>
2024-11-18 17:47:06 -0500

    Revert "Gitignore update and recache"
    This reverts commit aecc8ace1243f293be1166dc7c834b79fa6134e1.

---
+++ firmware/STM32 Drivers/AT24C04C/Debug/Drivers/AT24C04C/Src/subdir.mk
@@ -0,0 +1,27 @@
+################################################################################
+# Automatically-generated file. Do not edit!
+# Toolchain: GNU Tools for STM32 (12.3.rel1)
+################################################################################
+
+# Add inputs and outputs from these tool invocations to the build variables 
+C_SRCS += \
+../Drivers/AT24C04C/Src/stm32f1xx_at24c04c.c 
+
+OBJS += \
+./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.o 
+
+C_DEPS += \
+./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.d 
+
+
+# Each subdirectory must supply rules for building sources it contributes
+Drivers/AT24C04C/Src/%.o Drivers/AT24C04C/Src/%.su Drivers/AT24C04C/Src/%.cyclo: ../Drivers/AT24C04C/Src/%.c Drivers/AT24C04C/Src/subdir.mk
+	arm-none-eabi-gcc "$<" -mcpu=cortex-m3 -std=gnu11 -g3 -DDEBUG -DUSE_HAL_DRIVER -DSTM32F103xB -c -I../Core/Inc -I"C:/Users/ishan/OneDrive/Documents/MarsRover/maxwell/firmware/STM32 Drivers/AT24C04C/Drivers/AT24C04C/Inc" -I../Drivers/STM32F1xx_HAL_Driver/Inc -I../Drivers/STM32F1xx_HAL_Driver/Inc/Legacy -I../Drivers/CMSIS/Device/ST/STM32F1xx/Include -I../Drivers/CMSIS/Include -O0 -ffunction-sections -fdata-sections -Wall -fstack-usage -fcyclomatic-complexity -MMD -MP -MF"$(@:%.o=%.d)" -MT"$@" --specs=nano.specs -mfloat-abi=soft -mthumb -o "$@"
+
+clean: clean-Drivers-2f-AT24C04C-2f-Src
+
+clean-Drivers-2f-AT24C04C-2f-Src:
+	-$(RM) ./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.cyclo ./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.d ./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.o ./Drivers/AT24C04C/Src/stm32f1xx_at24c04c.su
+
+.PHONY: clean-Drivers-2f-AT24C04C-2f-Src
+
