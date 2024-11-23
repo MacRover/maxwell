9741d7e18207b154f5ba763287afe88f95ec51b3
John Cho <johnnychox@gmail.com>
2024-11-23 01:15:56 -0500

    fix indentation

--- firmware/STM32 Drivers/TMP_1075/Core/Src/tmp.c
+++ firmware/STM32 Drivers/TMP_1075/Core/Src/tmp.c
@@ -11,19 +11,19 @@ TMP_1075_HandleTypeDef h_tmp_1075;
 
 void MX_TMP_1075_Init()
 {
-	h_tmp_1075.__hi2c = &hi2c1;
-	h_tmp_1075.temp = 0.0;
-	h_tmp_1075.low_limit = 0.0;
-	h_tmp_1075.high_limit = 70.0;
+    h_tmp_1075.__hi2c = &hi2c1;
+    h_tmp_1075.temp = 0.0;
+    h_tmp_1075.low_limit = 0.0;
+    h_tmp_1075.high_limit = 70.0;
 
-	h_tmp_1075.conf.os = 0;
-	h_tmp_1075.conf.faults = 0; // 1 fault
-	h_tmp_1075.conf.polarity = 0;
-	h_tmp_1075.conf.tm = 0;
-	h_tmp_1075.conf.sd = 0;
+    h_tmp_1075.conf.os = 0;
+    h_tmp_1075.conf.faults = 0; // 1 fault
+    h_tmp_1075.conf.polarity = 0;
+    h_tmp_1075.conf.tm = 0;
+    h_tmp_1075.conf.sd = 0;
 
-	if (TMP_1075_Init(&h_tmp_1075) != TMP_1075_OK)
-	{
-		Error_Handler();
-	}
+    if (TMP_1075_Init(&h_tmp_1075) != TMP_1075_OK)
+    {
+        Error_Handler();
+    }
 }
