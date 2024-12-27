feab26a5dba59ae13b0ea6f56569a79a471bc5cf
chosterto <johnnychox@gmail.com>
2024-12-27 14:38:45 -0500

    Add xbox controller to bs_main.launch.py

--- ros_ws/src/bringup_py/launch/bs_main.launch.py
+++ ros_ws/src/bringup_py/launch/bs_main.launch.py
@@ -1,8 +1,9 @@
 import os
 import yaml
 from launch import LaunchDescription
-from launch.actions import DeclareLaunchArgument
+from launch.actions import (DeclareLaunchArgument, IncludeLaunchDescription)
 from launch.substitutions import LaunchConfiguration
+from launch.launch_description_sources import PythonLaunchDescriptionSource
 
 from ament_index_python.packages import get_package_share_directory
 
@@ -16,4 +17,17 @@ def generate_default_params() -> dict:
 def generate_launch_description():
     dp = generate_default_params()
 
-    return LaunchDescription()
+    xbox_controller = IncludeLaunchDescription(
+        PythonLaunchDescriptionSource([
+            os.path.join(get_package_share_directory("drive"), "launch"),
+            "/xbox_controller.launch.py"
+        ]),
+        launch_arguments={
+            "drive_mode": dp["drive"]["drive_mode"],
+            "can_rate": str(dp["drive"]["can_rate"]),
+        }.items()
+    )
+
+    ld = LaunchDescription()
+    ld.add_action(xbox_controller)
+    return ld
