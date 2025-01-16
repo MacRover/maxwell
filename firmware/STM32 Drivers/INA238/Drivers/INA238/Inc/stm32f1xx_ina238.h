727ea46877a88fdbddc0554328cd47a83261547e
Majd Aburas <aburasm@mcmaster.ca>
2025-01-15 20:13:33 -0500

    Implemented Read Current Method

--- firmware/STM32 Drivers/INA238/Drivers/INA238/Inc/stm32f1xx_ina238.h
+++ firmware/STM32 Drivers/INA238/Drivers/INA238/Inc/stm32f1xx_ina238.h
@@ -10,7 +10,9 @@
 
 #include "stm32f1xx.h"
 
-//Define register values
+#define INA238_CONFIG_ADDRESS 0x00
+#define INA238_ADC_CONFIG_ADDRESS 0x01
+#define INA238_CURRENT_ADDRESS 0x07
 
 typedef enum
 {
@@ -19,7 +21,6 @@ typedef enum
     INA_238_BUSY = 0x02U,
     INA_238_TIMEOUT = 0x03U
 } INA_238_StatusTypeDef;
-
 
 typedef struct
 {
@@ -36,7 +37,7 @@ typedef struct
 
 typedef struct
 {
-	//Enter key params here
+    // Enter key params here
 
     I2C_HandleTypeDef *I2C_HandlerInstance; // i2c instance
 
@@ -46,9 +47,9 @@ typedef struct
 
     uint8_t device_identifier;
 
-    float shunt_resistor;
+    double shunt_resistor;
 
-
+    double max_expected_current;
 
 } INA_238_InitTypeDef;
 
@@ -59,44 +60,39 @@ typedef enum
 {
     INA_238_STATE_RESET = 0x00U, /*!< Peripheral not Initialized                         */
     INA_238_STATE_READY = 0x01U, /*!< Peripheral Initialized and ready for use           */
-    INA_238_STATE_BUSY = 0x02U, /*!< an internal process is ongoing                     */
+    INA_238_STATE_BUSY = 0x02U,  /*!< an internal process is ongoing                     */
     INA_238_STATE_ERROR = 0x06U, /*!< SPI error state                                    */
 } INA_238_StateTypeDef;
 
 typedef struct
 {
-    INA_238_InitTypeDef Init; 
+    INA_238_InitTypeDef Init;
 
     INA_238_ConfRegister_TypeDef ConfigurationRegisters;
 
     volatile INA_238_StateTypeDef state;
 
-    //refer to section 8.2.2.5 of datasheet to see how these are converted
-    float voltage;
-    float current;
-    float power;
-    float diagnostic;
+    // refer to section 8.2.2.5 of datasheet to see how these are converted
+    double voltage;
+    double current;
+    double power;
+    double diagnostic;
 } INA_238_HandleTypeDef;
 
+INA_238_StatusTypeDef INA_238_Init(INA_238_HandleTypeDef *ina_238);
 
+INA_238_StatusTypeDef INA_238_DeInit(INA_238_HandleTypeDef *ina_238);
 
-INA_238_StatusTypeDef INA_238_Init(INA_238_HandleTypeDef* ina_238);
+INA_238_StatusTypeDef INA_238_ReadCurrent(INA_238_HandleTypeDef *ina_238);
+INA_238_StatusTypeDef INA_238_ReadVoltage(INA_238_HandleTypeDef *ina_238);
+INA_238_StatusTypeDef INA_238_ReadPower(INA_238_HandleTypeDef *ina_238);
+INA_238_StatusTypeDef INA_238_ReadDiagnostic(INA_238_HandleTypeDef *ina_238);
+INA_238_StatusTypeDef INA_238_WriteConfig(INA_238_HandleTypeDef *ina_238);
 
-INA_238_StatusTypeDef INA_238_DeInit(INA_238_HandleTypeDef* ina_238);
+uint16_t __i2c_set_register_pointer(INA_238_HandleTypeDef *ina_238, uint8_t register_address);
 
-INA_238_StatusTypeDef INA_238_ReadCurrent(INA_238_HandleTypeDef* ina_238);
-INA_238_StatusTypeDef INA_238_ReadVoltage(INA_238_HandleTypeDef* ina_238);
-INA_238_StatusTypeDef INA_238_ReadPower(INA_238_HandleTypeDef* ina_238);
-INA_238_StatusTypeDef INA_238_ReadDiagnostic(INA_238_HandleTypeDef* ina_238);
-INA_238_StatusTypeDef INA_238_WriteConfig(INA_238_HandleTypeDef* ina_238);
+uint16_t __i2c_read_register(INA_238_HandleTypeDef *ina_238, uint8_t *buffer);
 
-
-uint16_t __i2c_set_register_pointer(INA_238_HandleTypeDef* ina_238, uint8_t register_address);
-
-uint16_t __i2c_read_register(INA_238_HandleTypeDef* ina_238, uint8_t *buffer);
-
-uint16_t __i2c_write_register(INA_238_HandleTypeDef* ina_238, uint8_t register_address, uint8_t *data);
-
-
+uint16_t __i2c_write_register(INA_238_HandleTypeDef *ina_238, uint8_t register_address, uint8_t *data);
 
 #endif /* INA238_INC_INA238_H_ */
