859a83807afd1ef6d33877cf5d239207648ced9b
Ishan Vermani <ishanvermani65@gmail.com>
2025-01-28 16:56:38 -0500

    Status main. Incomplete

---
+++ ros_ws/src/viper-control/launch/viper_status_main.launch.py
@@ -0,0 +1,47 @@
+from launch import LaunchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+    # STATUS READER NODE
+    reader1_node = Node(
+        package="spidercan",
+        executable="reader.py",
+        name="viper1_reader",
+        parameters=[{
+            "topic": "/can/status/viper_can_in",
+            "channel": "can0",
+            "can_ids": [0x404FB00, 0x404FC00],
+            "can_masks": [0xFFFFF00, 0xFFFFF00]
+        }]
+    )
+
+    # CONFIG READER NODE
+    reader2_node = Node(
+        package="spidercan",
+        executable="reader.py",
+        name="viper2_reader",
+        parameters=[{
+            "topic": "/can/config/viper_can_in",
+            "channel": "can0",
+            "can_ids": [0x4040000],
+            "can_masks": [0xFFF8000]
+        }]
+    )
+
+    viper_status_node = Node(
+        package="rad_control",
+        executable="viper_status",
+        name="viper_status_node",
+        parameters=[{
+            "can_topic": "/can/status/rad_can_in",
+            "status_rate": 15
+        }]
+    )
+
+    return LaunchDescription(
+        [
+            reader1_node,
+            reader2_node,
+            rad_status_node,
+        ]
+    )
