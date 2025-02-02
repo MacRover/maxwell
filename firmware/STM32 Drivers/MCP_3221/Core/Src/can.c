b279b15afea365d98dce9540450f59cd6ba5f5da
Ishan Vermani <ishanvermani65@gmail.com>
2025-02-02 15:55:23 -0500

    Can communication for debugging

--- firmware/STM32 Drivers/MCP_3221/Core/Src/can.c
+++ firmware/STM32 Drivers/MCP_3221/Core/Src/can.c
@@ -19,8 +19,13 @@
 /* USER CODE END Header */
 /* Includes ------------------------------------------------------------------*/
 #include "can.h"
+#include "enc_dec_utils.h"
+
 
 /* USER CODE BEGIN 0 */
+
+VIPER_CAN_TypeDef viper_can;
+
 
 /* USER CODE END 0 */
 
@@ -37,23 +42,39 @@ void MX_CAN_Init(void)
   /* USER CODE BEGIN CAN_Init 1 */
 
   /* USER CODE END CAN_Init 1 */
-  hcan.Instance = CAN1;
-  hcan.Init.Prescaler = 16;
-  hcan.Init.Mode = CAN_MODE_NORMAL;
-  hcan.Init.SyncJumpWidth = CAN_SJW_1TQ;
-  hcan.Init.TimeSeg1 = CAN_BS1_1TQ;
-  hcan.Init.TimeSeg2 = CAN_BS2_1TQ;
-  hcan.Init.TimeTriggeredMode = DISABLE;
-  hcan.Init.AutoBusOff = DISABLE;
-  hcan.Init.AutoWakeUp = DISABLE;
-  hcan.Init.AutoRetransmission = DISABLE;
-  hcan.Init.ReceiveFifoLocked = DISABLE;
-  hcan.Init.TransmitFifoPriority = DISABLE;
+	hcan.Instance = CAN1;
+	hcan.Init.Prescaler = 4;
+	hcan.Init.Mode = CAN_MODE_NORMAL;
+	hcan.Init.SyncJumpWidth = CAN_SJW_2TQ;
+	hcan.Init.TimeSeg1 = CAN_BS1_15TQ;
+	hcan.Init.TimeSeg2 = CAN_BS2_2TQ;
+	hcan.Init.TimeTriggeredMode = DISABLE;
+	hcan.Init.AutoBusOff = DISABLE;
+	hcan.Init.AutoWakeUp = DISABLE;
+	hcan.Init.AutoRetransmission = ENABLE;
+	hcan.Init.ReceiveFifoLocked = DISABLE;
+	hcan.Init.TransmitFifoPriority = DISABLE;
   if (HAL_CAN_Init(&hcan) != HAL_OK)
   {
     Error_Handler();
   }
   /* USER CODE BEGIN CAN_Init 2 */
+
+  if (HAL_CAN_Start(&hcan) != HAL_OK)
+      {
+          /* Start Error */
+          Error_Handler();
+      }
+
+  viper_can.hcan = hcan;
+
+  viper_can.id = 0x14;
+
+  viper_can.TxHeader.RTR = CAN_RTR_DATA;
+  viper_can.TxHeader.IDE = CAN_ID_EXT;
+  viper_can.TxHeader.DLC = 0;
+  viper_can.TxHeader.TransmitGlobalTime = DISABLE;
+
 
   /* USER CODE END CAN_Init 2 */
 
@@ -116,5 +137,25 @@ void HAL_CAN_MspDeInit(CAN_HandleTypeDef* canHandle)
 }
 
 /* USER CODE BEGIN 1 */
+
+void MX_CAN_Broadcast_Double_Data(VIPER_CAN_TypeDef *viper_can_handle, double value, uint16_t message_id)
+{
+    encode_double_big_endian(value, &(viper_can_handle->TxData[0]));
+    viper_can_handle->TxHeader.DLC = sizeof(double); //float
+    viper_can_handle->TxHeader.ExtId = __encode_ext_can_id(viper_can_handle->id, message_id);
+
+    HAL_CAN_AddTxMessage(&(viper_can_handle->hcan), &(viper_can_handle->TxHeader),
+            viper_can_handle->TxData, &(viper_can_handle->TxMailbox));
+}
+
+uint32_t __encode_ext_can_id(uint8_t device_id, uint8_t message_id)
+{
+    // return a value that combines both the device ID and the
+    // message ID so that the message can be identified
+    return (CAN_MESSAGE_IDENTIFIER_VIPER << CAN_MESSAGE_IDENTIFIER_OFFSET) |
+            (CAN_MESSAGE_RESPONSE_VIPER << CAN_MESSAGE_RESPONSE_OFFSET) |
+            (message_id << CAN_MESSAGE_COMMAND_OFFSET) | (device_id << CAN_MESSAGE_DEVICE_ID_OFFSET);
+}
+
 
 /* USER CODE END 1 */
