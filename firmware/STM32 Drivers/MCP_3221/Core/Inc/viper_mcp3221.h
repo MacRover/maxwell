9ab2f394f8b842b5b2e3ba882def359fa108907e
Ali Naqvi <ali.naqvi8062@gmail.com>
2025-02-02 15:47:45 -0500

    Fixed syntac issues and verified code compilation

--- firmware/STM32 Drivers/MCP_3221/Core/Inc/viper_mcp3221.h
+++ firmware/STM32 Drivers/MCP_3221/Core/Inc/viper_mcp3221.h
@@ -13,10 +13,10 @@
 #include "mcp_3221.h"
 
 // Define VIPER-specific constants
-#define VIPER_SCALING_FACTOR        double(17.6)      // Voltage divider scaling factor
-#define VIPER_LOW_SENSE_RES         double(0.002)     // Sense resistor value in ohms for low power card
-#define VIPER_HIGH_SENSE_RES        double(0.001)     // Sense resistor value in ohms for high power card
-#define VIPER_VREF_MV               double(3300.0)    // Reference voltage in millivolts
+#define VIPER_SCALING_FACTOR        (17.6)      // Voltage divider scaling factor
+#define VIPER_LOW_SENSE_RES         (0.002)     // Sense resistor value in ohms for low power card
+#define VIPER_HIGH_SENSE_RES        (0.001)     // Sense resistor value in ohms for high power card
+#define VIPER_VREF_MV               (3300.0)    // Reference voltage in millivolts
 #define VIPER_ADC_ADDRESS           0x4D    		 // I2C address for the MCP3221 device
 
 // Function declarations
--- firmware/STM32 Drivers/MCP_3221/Drivers/MCP_3221/Src/mcp_3221.c
+++ firmware/STM32 Drivers/MCP_3221/Drivers/MCP_3221/Src/mcp_3221.c
@@ -8,12 +8,12 @@
 #include "mcp_3221.h"
 
 // DONE, need to revise. 
-MCP3221_StatusTypeDef MCP3221_Init(MCP3221_HandleTypeDef *mcp3221) {
+MCP_3221_StatusTypeDef MCP3221_Init(MCP3221_HandleTypeDef *mcp3221) {
 
     if (mcp3221 == NULL)
-        return MCP3221_ERROR;
+        return MCP_3221_ERROR;
     if (mcp3221->Init.hi2c == NULL)
-        return MCP3221_ERROR;
+        return MCP_3221_ERROR;
 
     mcp3221->i2c_address = (MCP3221_FIXED_ID << 3) | (mcp3221->Init.address_pins & 0x07);
 
@@ -21,42 +21,42 @@ MCP3221_StatusTypeDef MCP3221_Init(MCP3221_HandleTypeDef *mcp3221) {
     uint8_t dummy_data[MCP3221_DATA_BYTES] = {0};
     if (HAL_I2C_Master_Receive(mcp3221->Init.hi2c, (mcp3221->i2c_address << 1), dummy_data, MCP3221_DATA_BYTES, HAL_MAX_DELAY) != HAL_OK) {
         uint16_t error = HAL_I2C_GetError(mcp3221->Init.hi2c);
-        return (error == HAL_I2C_ERROR_AF) ? MCP3221_BUSY : MCP3221_ERROR;
+        return (error == HAL_I2C_ERROR_AF) ? MCP_3221_BUSY : MCP_3221_ERROR;
     }
 
-    return MCP3221_OK;
+    return MCP_3221_OK;
 }
 
-MCP3221_StatusTypeDef MCP3221_ReadVoltage(MCP3221_HandleTypeDef *mcp3221) {
-    if (__i2c_read_register(mcp3221) != MCP3221_OK)
+MCP_3221_StatusTypeDef MCP3221_ReadVoltage(MCP3221_HandleTypeDef *mcp3221) {
+    if (__i2c_read_register(mcp3221) != MCP_3221_OK)
         return MCP_3221_ERROR;
 
     mcp3221->voltage = ((float)mcp3221->adc_value / MCP3221_RESOLUTION) * mcp3221->Init.vref_mv;
-    return MCP3221_OK;
+    return MCP_3221_OK;
 }
 
 // DONE, need to revise. 
-MCP3221_StatusTypeDef MCP3221_ReadCurrent(MCP3221_HandleTypeDef *mcp3221){
+MCP_3221_StatusTypeDef MCP3221_ReadCurrent(MCP3221_HandleTypeDef *mcp3221){
 
-    if (__i2c_read_register(mcp3221) != MCP3221_OK)
+    if (__i2c_read_register(mcp3221) != MCP_3221_OK)
         return MCP_3221_ERROR;
 
     // may not need a temp, but i added it in anyways just in case.
     double temp_voltage;
     temp_voltage = ((float)mcp3221->adc_value / MCP3221_RESOLUTION) * mcp3221->Init.vref_mv;
     mcp3221->current = (temp_voltage/mcp3221->Init.sense_resistor_ohms) / mcp3221->Init.sense_resistor_ohms;
-    return MCP3221_OK;
+    return MCP_3221_OK;
 }
 
 // DONE, need to revise. 
-static MCP3221_StatusTypeDef __i2c_read_register(MCP3221_HandleTypeDef *mcp3221){
+MCP_3221_StatusTypeDef __i2c_read_register(MCP3221_HandleTypeDef *mcp3221){
     uint8_t data[MCP3221_DATA_BYTES];  // data [0] is the upper byte which will have its first 4 bits masked
     if (HAL_I2C_Master_Receive(mcp3221->Init.hi2c, (mcp3221->i2c_address << 1) | 1, data, MCP3221_DATA_BYTES, HAL_MAX_DELAY) != HAL_OK) {
         uint16_t error = HAL_I2C_GetError(mcp3221->Init.hi2c);
-        return (error == HAL_I2C_ERROR_AF) ? MCP3221_BUSY : MCP3221_ERROR;
+        return (error == HAL_I2C_ERROR_AF) ? MCP_3221_BUSY : MCP_3221_ERROR;
     }
     mcp3221->adc_value = ((data[0] & 0x0F) << 8) | data[1];
-    return MCP3221_OK;
+    return MCP_3221_OK;
 }
 
 
