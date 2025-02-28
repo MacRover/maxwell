57e4ec1ea951e5c2740a00e989c6bad2b4b1ded3
RutheniumVI <veeresh.532000@gmail.com>
2025-02-27 23:56:34 -0500

    modified rad_converter for new arm controller

--- ros_ws/src/arm_controller/src/rad_converter.cpp
+++ ros_ws/src/arm_controller/src/rad_converter.cpp
@@ -1,28 +1,23 @@
 #include "arm_controller/rad_converter.hpp"
 
-void RAD_Converter::_callback(const custom_interfaces::msg::RadStatus::SharedPtr msg, int id)
-{
-  joint_trajectory_.points[0].positions[id] = msg->angle;
-  joint_trajectory_.points[0].velocities[id] = msg->speed;
-  pub_->publish(joint_trajectory_);    
-}
-
 RAD_Converter::RAD_Converter() : Node("rad_arm_converter"),
                                  joints_(NUM_JOINTS, 0.0)
 {
-  joint_trajectory_.joint_names = { "arm_base_joint", "arm_shoulder_joint", "arm_elbow_joint", "arm_wrist_joint", "gripper_joint"};
-  joint_trajectory_.points.resize(1);
-  joint_trajectory_.points[0].time_from_start = rclcpp::Duration(1, 0);
-  joint_trajectory_.points[0].positions = {0.0, 0.0, 0.0, 0.0, 0.0};
-  joint_trajectory_.points[0].velocities = {0.0, 0.0, 0.0, 0.0, 0.0};
+  joint_state_.name = { "arm_base_joint", "arm_shoulder_joint", "arm_elbow_joint", "arm_wrist_joint", "gripper_joint"};
+  joint_state_.position.assign(5, 0);;
+  joint_state_.velocity.assign(5, 0);;
 
   for (int i = 0; i < NUM_JOINTS; i++)
   {
     sub_[i] = this->create_subscription<custom_interfaces::msg::RadStatus>(
         "/joint" + std::to_string(i) + "/rad_status", 10,
-        [&](const custom_interfaces::msg::RadStatus::SharedPtr msg){ _callback(msg, i); });
+        [this, i](const custom_interfaces::msg::RadStatus::SharedPtr msg){
+          joint_state_.position[i] = msg->angle;
+          joint_state_.velocity[i] = msg->speed;
+          pub_->publish(joint_state_);    
+        });
   }
-  pub_ = this->create_publisher<trajectory_msgs::msg::JointTrajectory>("/arm_controller/joint_trajectory", 1);
+  pub_ = this->create_publisher<sensor_msgs::msg::JointState>("/arm/rad/joint_states", 1);
 
 }
 
