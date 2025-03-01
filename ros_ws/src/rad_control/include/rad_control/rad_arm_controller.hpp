4952b63b47ca7fdb8b16edd7a16d1a4e3422a4ea
WillScott9214 <will.scott9214@gmail.com>
2025-02-28 20:04:20 -0500

    Updated For Testing

--- ros_ws/src/rad_control/include/rad_control/rad_arm_controller.hpp
+++ ros_ws/src/rad_control/include/rad_control/rad_arm_controller.hpp
@@ -1,9 +1,10 @@
 #pragma once
 #include <cstdint>
 #include "rclcpp/rclcpp.hpp"
-#include "custom_interfaces/msg/ca_nraw.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "rad.hpp"
+#include "custom_interfaces/msg/ca_nraw.hpp"
+
 
 
 class RAD_Arm_Controller : public rclcpp::Node
@@ -11,13 +12,21 @@ class RAD_Arm_Controller : public rclcpp::Node
 public:
   RAD_Arm_Controller();
 private:
-  void _callback(const JointState& msg);
-  void publish_joint_angles(const JointState& msg);   
+  void _callback(const sensor_msgs::msg::JointState::SharedPtr msg);
+  void publish_joint_angles();   
   void _timer_callback(void);
+  void _publish_to_can();
 
-  std::shared_ptr<rclcpp::Subscription<JointState> > sub_;
-  std::shared_ptr<rclcpp::Publisher<JointState> > pub_;
+  std::shared_ptr<rclcpp::Publisher<CANraw> > can_pub_;
+  std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::JointState> > sub_;
+  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::JointState> > pub_;
   std::shared_ptr<rclcpp::TimerBase> timer_; 
 
-  RAD rad_base_arm, rad_elbow_arm, rad_shoulder_arm, rad_pitch_arm, rad_wrist_arm, rad_gripper_arm; 
+  CANraw can_base, can_shoulder, can_gripper, can_elbow, can_wrist, can_pitch;
+
+
+  RAD rad_base_arm, rad_pitch_arm, rad_gripper_arm, rad_shoulder_arm, rad_elbow_arm, rad_wrist_arm; 
+
+  uint16_t sleep_msec;
+
 };
