9559b111c8487cf8f3528ff66f19f1576a170518
WillScott9214 <will.scott9214@gmail.com>
2025-03-06 22:58:11 -0500

    Fixed issues with rad_arm_controller

--- ros_ws/src/rad_control/include/rad_control/rad_arm_controller.hpp
+++ ros_ws/src/rad_control/include/rad_control/rad_arm_controller.hpp
@@ -1,26 +1,22 @@
 #pragma once
 #include <cstdint>
 #include "rclcpp/rclcpp.hpp"
+#include "custom_interfaces/msg/ca_nraw.hpp"
 #include "sensor_msgs/msg/joint_state.hpp"
 #include "rad.hpp"
-#include "custom_interfaces/msg/ca_nraw.hpp"
 
-
+using namespace custom_interfaces::msg;
 
 class RAD_Arm_Controller : public rclcpp::Node
 {
 public:
   RAD_Arm_Controller();
 private:
-  void _callback(const sensor_msgs::msg::JointState::SharedPtr msg);
-  void publish_joint_angles();   
-  void _timer_callback(void);
+  void _callback(const sensor_msgs::msg::JointState& msg);
   void _publish_to_can();
 
   std::shared_ptr<rclcpp::Publisher<CANraw> > can_pub_;
   std::shared_ptr<rclcpp::Subscription<sensor_msgs::msg::JointState> > sub_;
-  std::shared_ptr<rclcpp::Publisher<sensor_msgs::msg::JointState> > pub_;
-  std::shared_ptr<rclcpp::TimerBase> timer_; 
 
   CANraw can_base, can_shoulder, can_gripper, can_elbow, can_wrist, can_pitch;
 
--- ros_ws/src/rad_control/src/rad_arm_controller.cpp
+++ ros_ws/src/rad_control/src/rad_arm_controller.cpp
@@ -1,46 +1,66 @@
 #include <cstdio>
 #include <chrono>
-#include "rad_control/RAD_Arm_Controller.hpp"
-#include "sensor_msgs/msg/joint_state.hpp"
+#include "rad_control/rad_arm_controller.hpp"
 using std::placeholders::_1;
 
 RAD_Arm_Controller::RAD_Arm_Controller() : 
   Node("rad_arm_controller"),
-  rad_base_arm(&can_base, RAD__ARM__BASE), //Does RAD__ARM__BASE, etc need can ID's? 
+  rad_base_arm(&can_base, RAD__ARM__BASE), 
   rad_pitch_arm(&can_pitch, RAD__ARM__PITCH),
   rad_gripper_arm(&can_gripper, RAD__ARM__GRIPPER),
   rad_shoulder_arm(&can_shoulder, RAD__ARM__SHOULDER),
   rad_elbow_arm(&can_elbow, RAD__ARM__ELBOW),
   rad_wrist_arm(&can_wrist, RAD__ARM__WRIST)
 {
+  this->declare_parameter("can_rate", 10);
+  sleep_msec = (uint16_t)(1000.0 / (4.0 * (float)this->get_parameter("can_rate").as_int()));
   can_pub_ = this->create_publisher<CANraw>("/can/can_out", 10);
-
+  std::cout<<"H";
   sub_ = this->create_subscription<sensor_msgs::msg::JointState>(
-    "/modules_command", 10, std::bind(&RAD_Arm_Controller::_callback, this, std::placeholders::_1)
+    "/joint_states", 10, std::bind(&RAD_Arm_Controller::_callback, this, _1)
   );
 
-  pub_ = this->create_publisher<sensor_msgs::msg::JointState>(
+ /* pub_ = this->create_publisher<sensor_msgs::msg::JointState>(
     "/arm/can/can_out", 10
-  ); 
+  ); */
 
-  timer_ = this->create_wall_timer(
+  /*timer_ = this->create_wall_timer(
     std::chrono::milliseconds(100), std::bind(&RAD_Arm_Controller::_timer_callback, this) 
-  );
+  );*/
 }
-void RAD_Arm_Controller::_callback(const sensor_msgs::msg::JointState::SharedPtr msg)
+void RAD_Arm_Controller::_publish_to_can()
 {
-  float base_angle = msg->position[0]; 
-  float shoulder_angle = msg->position[1];
-  float elbow_angle = msg->position[2];
-  float ls = msg->position[3];
-  float rs = msg->position[4];
-  float gripper_angle = msg->position[5]; 
+   rclcpp::Rate rate{std::chrono::milliseconds(sleep_msec)};
 
-  float theta_c = ((L_max - L_min)*360)/2.55; //Modifier for the output angles
+    can_pub_->publish(can_base);
+    rate.sleep();
+    can_pub_->publish(can_pitch);
+    rate.sleep();
+    can_pub_->publish(can_gripper);
+    rate.sleep();
+    can_pub_->publish(can_shoulder);
+    rate.sleep();
+    can_pub_->publish(can_elbow);
+    rate.sleep();
+    can_pub_->publish(can_wrist);
+}
+void RAD_Arm_Controller::_callback(const sensor_msgs::msg::JointState& msg)
+{
+  float base_angle = msg.position[0]; 
+  float  shoulder_angle = msg.position[1];
+  float elbow_angle = msg.position[2];
+  float ls = msg.position[3];
+  float rs = msg.position[4];
+  float gripper_angle = msg.position[5]; 
+
+  //float theta_c = ((L_max - L_min)*360)/2.55; //Modifier for the output angles
 
   float pitch_angle = (ls + rs)/2;
   float wrist_angle = (ls - rs)/2; 
   
+  // Pretty sure isolating for Theta_M gives: 
+  // (-360/2.54)*(-L_min-a^2-b^2+sqrt(cos_inv*2*a*b*(theta_s+theta_1-theta_2))) 
+  // theta_m, max = ((L_max - L_min)*360)/2.54 
 
   rad_base_arm.set_target_angle(base_angle);
   rad_shoulder_arm.set_target_angle(shoulder_angle);
@@ -49,31 +69,19 @@ void RAD_Arm_Controller::_callback(const sensor_msgs::msg::JointState::SharedPtr
   rad_wrist_arm.set_target_angle(wrist_angle);
   rad_gripper_arm.set_target_angle(gripper_angle);
 
+  this->_publish_to_can(); 
 }
 
-void RAD_Arm_Controller::_timer_callback()
+
+/*void RAD_Arm_Controller::_timer_callback(void)
 {
-
-}
-
-void RAD_Arm_Controller::publish_joint_angles()
-{
-  
-  //Is this doubling up the info 
-    rad_base_arm.get_target_angle();
-    rad_shoulder_arm.get_target_angle();
-    rad_elbow_arm.get_target_angle(); 
-    rad_pitch_arm.get_target_angle(); 
-    rad_wrist_arm.get_target_angle(); 
-    rad_gripper_arm.get_target_angle();
-
     can_pub_->publish(can_base);
     can_pub_->publish(can_pitch);
-    can_pub_->publish(can_gripper);
     can_pub_->publish(can_shoulder);
     can_pub_->publish(can_elbow);
     can_pub_->publish(can_wrist);
-}
+    can_pub_->publish(can_gripper);
+}*/
 int main(int argc, char ** argv)
 {
   (void) argc;
