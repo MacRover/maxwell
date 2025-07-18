cd175dd6554942ddd69c73ad411a8f870a4a16a6
Ishan Vermani <ishanvermani65@gmail.com>
2025-07-17 23:30:20 -0600

    complie fix

--- ros_ws/src/rad_control/src/rad_science_controller.cpp
+++ ros_ws/src/rad_control/src/rad_science_controller.cpp
@@ -58,7 +58,7 @@ int main(int argc, char ** argv)
             base = 16;
             in = in.substr(1);
             }
-            rad_id = std::stoi(in, 0, 10);
+            rad_id = std::stoi(in, 0, base);
             rad.set_can_id(rad_id);
             )
             
